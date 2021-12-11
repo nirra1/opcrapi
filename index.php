0f1d29e8c3e24ae31bc620b747f308835e4c6793
nirra1 <40904847+nirra1@users.noreply.github.com>
2021-12-11 12:56:34 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 
 header("Access-Control-Allow-Origin: *");
+ header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 $api_url = "http://ecommerceapi2021.000webhostapp.com/opcr/api.php";
 
 
